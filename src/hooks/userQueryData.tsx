bb03eeb2d2ed070cbb429db0eb4f11295e030130
Yu Guo <yu.guo4@griffithuni.edu.au>
2025-01-22 14:34:25 +1000

    add new function useUerQueryData

---
+++ src/hooks/userQueryData.tsx
@@ -0,0 +1,19 @@
+import {
+  useQuery,
+  Enabled,
+  QueryFunction,
+  QueryKey,
+} from '@tanstack/react-query';
+
+export const useUserQueryData = (
+  queryKey: QueryKey,
+  queryFn: QueryFunction,
+  enabled: Enabled
+) => {
+  const { data, isFetching, isPending, isFetched, refetch } = useQuery({
+    queryKey,
+    queryFn,
+    enabled,
+  });
+  return { data, isFetching, isPending, isFetched, refetch };
+};
