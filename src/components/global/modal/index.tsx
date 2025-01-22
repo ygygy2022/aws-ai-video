83d7e6de113ff6505c747ef2cc387f99f2613e9a
Yu Guo <yu.guo4@griffithuni.edu.au>
2025-01-22 15:05:49 +1000

    add modal component

---
+++ src/components/global/modal/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+type Props = {
+  trigger: React.ReactNode;
+  children: React.ReactNode;
+  title: string;
+  description: string;
+  className?: string;
+};
+
+const Modal = ({ trigger, children, title, description, className }: Props) => {
+  return (
+    <Dialog>
+      <DialogTrigger className={className} asChild>
+        {trigger}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default Modal;
